c4a334256ff0f01be02d4a984822775fb3491a1e
= <=>
2020-09-24 16:10:49 +0100

    task 3

--- main.js
+++ main.js
@@ -21,3 +21,16 @@ function addList() {
 }
 
 buttonVar.addEventListener("click", addList);
+
+// Task 3
+const buttonDaily = document.querySelector("#dailyTasks");
+function loopTasks(){
+  for(let index= 0; index< dailyToDos.length; index ++){
+    const lis = document.createElement("li");
+    lis.innerText =dailyToDos[index];
+    ul.appendChild(lis);
+    console.log('index');
+  };
+ 
+}
+buttonDaily.addEventListener("click", loopTasks);
