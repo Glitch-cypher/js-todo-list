d721ee4b74eb5f1cdff0a06b47feec5c2bf4f6ef
= <=>
2020-09-24 15:56:00 +0100

    task 2 complete

--- main.js
+++ main.js
@@ -5,3 +5,19 @@ const dailyToDos = [
   "Eat Lunch",
   "Eat Dinner",
 ];
+
+let buttonVar = document.querySelector("button");
+const input = document.querySelector("input");
+const ul = document.querySelector("ul");
+
+function addList() {
+  const li = document.createElement("li");
+  li.innerText = input.value;
+  if(input.value === ""){
+    return;
+  }
+  ul.appendChild(li);
+  input.value = "";
+}
+
+buttonVar.addEventListener("click", addList);
